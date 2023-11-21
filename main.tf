6679dd946e44e9e99ebd95e7bd47d3074b285d27
Jean-Quenault <jeanform@proton.me>
2023-11-21 11:25:27 +0100

    ami update

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_ec2_instance" {
-    ami = "ami-07c1207a9d40bc3bd"
+    ami = "ami-0302f42a44bf53a45"
     instance_type = "t2.micro"
     tags = {
         Name = "terraform test"
