f42f693c1a71745bf8c005b2d64538c7566efc0a
Jean-Quenault <jeanform@proton.me>
2023-11-22 12:02:45 +0100

    Update of the front launch template

--- modules/front/lt.tf
+++ modules/front/lt.tf
@@ -30,16 +30,9 @@ resource "aws_launch_template" "front" {
     security_groups = [aws_security_group.front_asg.id]
   }
 
-    /*user_data = base64encode(<<EOF
-        #!/bin/bash
-        aws ecr get-login-password --region eu-west-3 | docker login --username AWS --password-stdin 980377181750.dkr.ecr.eu-west-3.amazonaws.com
-        docker run -d -p 80:80 980377181750.dkr.ecr.eu-west-3.amazonaws.com/front:13
-    EOF
-    )*/
-
     user_data = base64encode(<<EOF
         #!/bin/bash
-        aws ecr get-login-password --region ${var.region}| docker login --username AWS --password-stdin ${var.ecr_url}
+        aws ecr get-login-password --region ${var.region} | docker login --username AWS --password-stdin ${var.ecr_url}
         docker run -d -p ${var.front_port}:80 ${var.ecr_url}/${var.front_image}:${var.front_image_tag}
     EOF
     )
