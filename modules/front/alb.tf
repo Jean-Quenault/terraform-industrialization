0b0c5588373112c6468efdf4580ecfc50436842e
Jean-Quenault <jeanform@proton.me>
2023-11-23 21:21:40 +0100

    Load balancer update

---
+++ modules/front/alb.tf
@@ -0,0 +1,61 @@
+data "aws_acm_certificate" "cert_front" {
+  domain   = "front.jeanops.net"
+  statuses = ["ISSUED"]
+}
+
+resource "aws_lb" "front_lb" {
+  name               = "front_lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.front_lb.id]
+  subnets            = [aws_subnet.euw3_az2.id, aws_subnet.euw3_az1.id, aws_subnet.euw3_az3.id]
+  enable_deletion_protection = false
+}
+
+resource "aws_subnet" "euw3_az2" {
+  vpc_id            = var.vpc_id
+  availability_zone = "eu-west-3b"
+}
+
+resource "aws_subnet" "euw3_az1" {
+  vpc_id            = var.vpc_id
+  availability_zone = "eu-west-3a"
+}
+
+resource "aws_subnet" "euw3_az3" {
+  vpc_id            = var.vpc_id
+  availability_zone = "eu-west-3c"
+}
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.front_lb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  certificate_arn   = data.aws_acm_certificate.cert_front.arn
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.front_tg.arn
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.front_lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+      path        = "/#{path}"
+      query       = "#{query}"
+    }
+  }
+}
+
+resource "aws_lb_target_group" "front_tg" {
+  
+}
