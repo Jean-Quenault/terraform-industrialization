467acae58a13bfaea31a5bce8a36b55c9ca85573
Jean-Quenault <jeanform@proton.me>
2023-11-24 10:10:38 +0100

    Update name front

--- modules/front/alb.tf
+++ modules/front/alb.tf
@@ -7,7 +7,7 @@ resource "aws_lb" "front" {
   name               = "front"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.front.id]
+  security_groups    = [aws_security_group.front_lb.id]
   subnets            = [aws_subnet.euw3_az2.id, aws_subnet.euw3_az1.id, aws_subnet.euw3_az3.id]
   enable_deletion_protection = false
 }
