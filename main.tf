f0cc517cd481beb399fbb20e9c0942e94e1b79d0
Jean-Quenault <jeanform@proton.me>
2023-11-24 15:53:39 +0100

    Variable update

--- main.tf
+++ main.tf
@@ -10,8 +10,8 @@ module "front" {
   source            = "./modules/front"
   vpc_id            = var.vpc_id
   region            = var.region
-  ecr_front_url     = var.ecr_front_url
-  certificate_front = var.certificate_front
+  ecr_front_url     = "980377181750.dkr.ecr.eu-west-3.amazonaws.com"
+  certificate_front = "arn:aws:acm:eu-west-3:980377181750:certificate/dc5fe3dd-2d06-4dad-a754-9ad14ee334cd"
   subnet            = var.subnet
   front_port        ="80"
   front_image       ="front"
@@ -23,8 +23,8 @@ module "back" {
   region            = var.region
   vpc_id            = var.vpc_id
   subnet            = var.subnet
-  ecr_back_url      = var.ecr_back_url
-  back_port        ="4000"
-  back_image       ="back"
-  back_image_tag   ="13"
+  ecr_back_url      = "980377181750.dkr.ecr.eu-west-3.amazonaws.com/back"
+  back_port         = "4000"
+  back_image        = "back"
+  back_image_tag    = "13"
 }
--- variable.tf
+++ variable.tf
@@ -14,23 +14,3 @@ variable "subnet" {
   description = "Front and back load balancer subnet"
   type        = string
 }
-
-// Front 
-
-variable "ecr_front_url" {
-  description = "ECR front URL"
-  type        = string
-}
-
-variable "certificate_front" {
-  description = "Front certificate ARN"
-  type        = string
-}
-
-
-// Back 
-
-variable "ecr_back_url" {
-  description = "ECR front URL"
-  type        = string
-}
