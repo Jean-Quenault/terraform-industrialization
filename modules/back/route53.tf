6cc97746877a3eee81ad485823233d2ac419ed06
Jean-Quenault <jeanform@proton.me>
2023-11-27 16:08:07 +0100

    Route53 Back

---
+++ modules/back/route53.tf
@@ -0,0 +1,11 @@
+resource "aws_route53_record" "back" {
+    zone_id = "Z06335482BEM13BVT0JEQ"
+    name    = "backend.jeanops.net"
+    type    = "A"
+
+    alias {
+        name                   = aws_lb.back.dns_name
+        zone_id                = aws_lb.back.zone_id
+        evaluate_target_health = true
+    }
+}
