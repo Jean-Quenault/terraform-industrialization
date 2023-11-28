d50ba92bc7318fa757794d437d8e16be188a2659
Jean-Quenault <jeanform@proton.me>
2023-11-28 13:54:02 +0100

    Correction of ECR front URL

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ module "front" {
   source            = "./modules/front"
   vpc_id            = var.vpc_id
   region            = var.region
-  ecr_front_url     = "980377181750.dkr.ecr.eu-west-3.amazonaws.com"
+  ecr_front_url     = "980377181750.dkr.ecr.eu-west-3.amazonaws.com/front"
   certificate_front = "arn:aws:acm:eu-west-3:980377181750:certificate/dc5fe3dd-2d06-4dad-a754-9ad14ee334cd"
   subnets            = [var.subnet1, var.subnet2, var.subnet3]
   front_port        ="80"
