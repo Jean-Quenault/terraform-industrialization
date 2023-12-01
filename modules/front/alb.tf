155b2680c5da01cbccf89197973728e969fd9a46
Jean-Quenault <jeanform@proton.me>
2023-12-01 11:08:25 +0100

    Change back image tag to 14

--- modules/front/alb.tf
+++ modules/front/alb.tf
@@ -18,7 +18,7 @@ resource "aws_lb_listener" "https" {
   port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
-  certificate_arn   = var.certificate_front 
+  certificate_arn   = var.certificate_front
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.fronttg.arn
