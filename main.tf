3941ada107afda29764f77d9082dd1674b59c6e8
Jean-Quenault <jeanform@proton.me>
2023-12-01 15:56:48 +0100

    Comments update

--- main.tf
+++ main.tf
@@ -2,10 +2,6 @@ provider "aws" {
     region = "eu-west-3"
 }
 
-
-# Variable "vpc_id" is passed to the module "front". Variable declared in ./variable.tf and defined in ./terraform.ttvars.
-# "Front" module received this variable in his own ./modules/front/variable.tf file.
-
 module "front" {
   source            = "./modules/front"
   vpc_id            = var.vpc_id
--- variable.tf
+++ variable.tf
@@ -1,5 +1,3 @@
-# Main declaration of vpc_id.
-
 variable "vpc_id" {
   description = "ID of the choosen VPC"
   type        = string
